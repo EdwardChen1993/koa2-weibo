f612555df455fb26e96ae042bf17091b22763347
edward_chen <edward_chen@zc-campus.com>
2020-06-11 22:49:59 +0800

    feature: 完成node连接redis

---
+++ src/utils/env.js
@@ -0,0 +1,14 @@
+/**
+ * @description 环境变量
+ * @author 泽华
+ */
+
+const app = require('../app');
+const ENV = app.env;
+
+module.exports = {
+    isDev: ENV === 'development',
+    notDev: ENV !== 'development',
+    isProd: ENV === 'production',
+    notProd: ENV !== 'production'
+}
