2674bb84ae40a93461298f626dca29946cccee73
edward_chen <872990547@qq.com>
2020-06-15 21:10:22 +0800

    feature: 登录接口

--- src/controller/user.js
+++ src/controller/user.js
@@ -5,7 +5,7 @@
 
 const { getUserInfo, createUser } = require('../services/user');
 const { SuccessModel, ErrorModel } = require('../model/ResModel');
-const { registerUserNameNotExistInfo, registerUserNameExistInfo, registerFailInfo } = require('../model/ErrorInfo');
+const { registerUserNameNotExistInfo, registerUserNameExistInfo, registerFailInfo, loginFailInfo } = require('../model/ErrorInfo');
 const doCrypto = require('../utils/cryp');
 
 /**
@@ -48,4 +48,23 @@ async function register({ userName, password, gender }) {
     }
 }
 
-module.exports = { isExist, register }
+/**
+ * 登录
+ * @param {Obejct} ctx koa2 ctx
+ * @param {String} userName 用户名
+ * @param {String} password 密码
+ */
+async function login(ctx, userName, password) {
+    const userInfo = await getUserInfo(userName, doCrypto(password));
+    if (!userInfo) {
+        // 登录失败
+        return new ErrorModel(loginFailInfo);
+    }
+    // 登录成功
+    if(ctx.session.userInfo == null) {
+        ctx.session.userInfo = userInfo;
+    }
+    return new SuccessModel();
+}
+
+module.exports = { isExist, register, login }
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -4,7 +4,7 @@
  */
 
 const router = require('koa-router')();
-const { isExist, register } = require('../../controller/user');
+const { isExist, register, login } = require('../../controller/user');
 const uesrValidate = require('../../validator/user');
 const { genValidator } = require('../../middlewares/validator');
 
@@ -20,6 +20,12 @@ router.post('/register', genValidator(uesrValidate), async (ctx, next) => {
 router.post('/isExist', async (ctx, next) => {
     const { userName } = ctx.request.body;
     ctx.body = await isExist(userName);
+})
+
+// 登录
+router.post('/login', async (ctx, next) => {
+    const { userName, password } = ctx.request.body;
+    ctx.body = await login(ctx, userName, password);
 })
 
 module.exports = router;
