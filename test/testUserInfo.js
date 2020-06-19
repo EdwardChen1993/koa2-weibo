4ee8aabb8bb2356d7ece428d1f7b302d995bda48
edward_chen <872990547@qq.com>
2020-06-19 18:37:01 +0800

    test: 关注和取消关注

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -6,6 +6,11 @@
 /* cookie是用户的敏感信息，此处只能是测试用户的cookie
 每次测试用户重新登录，都需要更新这里的cookie */
 module.exports = {
-    COOKIE: 'weibo.sid=LVjb33ZcpxxrUliSR6Kny8W5MCcxJGrK; weibo.sid.sig=GJZjynHtYtvj7GxqDwr-QrmCZas',
-    USER_NAME: 'lisi'
+    L_ID: 1,
+    L_COOKIE: 'weibo.sid=7AEX_C2si2hijJ1saeSbFaZYy0zoUMXE; weibo.sid.sig=WF08khnGoKzhsF0coZ00X_P5ZHY',
+    L_USER_NAME: 'lisi',
+    
+    Z_ID: 2,
+    Z_COOKIE: 'weibo.sid=9revU06h98WX0L_OTxJOGjtwetvTfKYU; weibo.sid.sig=dnObOM9oYjR7XBKK-Xs7rI1_eY0',
+    Z_USER_NAME: 'zhangsan',
 }
