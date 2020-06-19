e35569eee166db683e2fbaa65109ab8fe8a99145
edward_chen <872990547@qq.com>
2020-06-19 22:59:57 +0800

    test: 首页

--- src/routes/api/blog-home.js
+++ src/routes/api/blog-home.js
@@ -23,7 +23,7 @@ router.post('/create', loginCheck, genValidator(blogValidate), async (ctx, next)
 router.get('/loadMore/:pageIndex', loginCheck, async (ctx, next) => {
     let { pageIndex } = ctx.params;
     pageIndex = parseInt(pageIndex);  // 转换 number 类型
-    const {id:userId} = ctx.session.userInfo;
+    const { id: userId } = ctx.session.userInfo;
     const result = await getHomeBlogList(userId, pageIndex);
     // 渲染模板
     result.data.blogListTpl = getBlogListStr(result.data.blogList);
